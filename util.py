4104e12fdb1a1e36f00ae1d367d3b07a94798ea5
Jason Stumbaugh <jason.stumbaugh@orm-tech.com>
2017-10-19 21:22:53 -0500

    Comment out prints and uneccessary calculations

--- util.py
+++ util.py
@@ -29,7 +29,7 @@ class Util:
         my_points_by_season, elo_points_by_season = {}, {}
 
         forecasted_games = [g for g in games if g['result1'] != None]
-        upcoming_games = [g for g in games if g['result1'] == None and 'my_prob1' in g]
+        # upcoming_games = [g for g in games if g['result1'] == None and 'my_prob1' in g]
 
         # Evaluate forecasts and group by season
         for game in forecasted_games:
@@ -39,21 +39,21 @@ class Util:
                 continue
 
             # Don't count the 2017 NE/KC game because it wasn't included in our game
-            if game['date'] == '2017-09-07':
-              continue
+            # if game['date'] == '2017-09-07':
+            #   continue
 
             if game['season'] not in elo_points_by_season:
-                elo_points_by_season[game['season']] = 0.0
+                # elo_points_by_season[game['season']] = 0.0
                 my_points_by_season[game['season']] = 0.0
 
             # Calculate elo's points for game
-            rounded_elo_prob = round(game['elo_prob1'], 2)
-            elo_brier = (rounded_elo_prob - game['result1']) * (rounded_elo_prob - game['result1'])
-            elo_points = 25 - (100 * elo_brier)
-            elo_points = round(elo_points + 0.001 if elo_points < 0 else elo_points, 1) # Round half up
-            if game['playoff'] == 1:
-                elo_points *= 2
-            elo_points_by_season[game['season']] += elo_points
+            # rounded_elo_prob = round(game['elo_prob1'], 2)
+            # elo_brier = (rounded_elo_prob - game['result1']) * (rounded_elo_prob - game['result1'])
+            # elo_points = 25 - (100 * elo_brier)
+            # elo_points = round(elo_points + 0.001 if elo_points < 0 else elo_points, 1) # Round half up
+            # if game['playoff'] == 1:
+            #     elo_points *= 2
+            # elo_points_by_season[game['season']] += elo_points
 
             # Calculate my points for game
             rounded_my_prob = round(game['my_prob1'], 2)
@@ -65,17 +65,18 @@ class Util:
             my_points_by_season[game['season']] += my_points
 
         # Print individual seasons
-        for season in my_points_by_season:
-            print("In %s, your forecasts would have gotten %s points. Elo got %s points." % (season, round(my_points_by_season[season], 2), round(elo_points_by_season[season], 2)))
+        # for season in my_points_by_season:
+        #     print("In %s, your forecasts would have gotten %s points. Elo got %s points." % (season, round(my_points_by_season[season], 2), round(elo_points_by_season[season], 2)))
 
         # Show overall performance
         my_avg = sum(my_points_by_season.values())/len(my_points_by_season.values())
-        elo_avg = sum(elo_points_by_season.values())/len(elo_points_by_season.values())
-        print("\nOn average, your forecasts would have gotten %s points per season. Elo got %s points per season.\n" % (round(my_avg, 2), round(elo_avg, 2)))
+        return my_avg
+        # elo_avg = sum(elo_points_by_season.values())/len(elo_points_by_season.values())
+        # print("\nOn average, your forecasts would have gotten %s points per season. Elo got %s points per season.\n" % (round(my_avg, 2), round(elo_avg, 2)))
 
         # Print forecasts for upcoming games
-        if len(upcoming_games) > 0:
-            print("Forecasts for upcoming games:")
-            for game in upcoming_games:
-                print("%s\t%s vs. %s\t\t%s%% (Elo)\t\t%s%% (You)" % (game['date'], game['team1'], game['team2'], int(round(100*game['elo_prob1'])), int(round(100*game['my_prob1']))))
-            print("")
+        # if len(upcoming_games) > 0:
+        #     print("Forecasts for upcoming games:")
+        #     for game in upcoming_games:
+        #         print("%s\t%s vs. %s\t\t%s%% (Elo)\t\t%s%% (You)" % (game['date'], game['team1'], game['team2'], int(round(100*game['elo_prob1'])), int(round(100*game['my_prob1']))))
+        #     print("")
